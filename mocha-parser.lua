588a636fb958c20c168a79df27c0fc32d94b4739
torque <lulzbot3000@gmail.com>
2011-10-22 15:58:17 -0700

    Possible VSfilter compatibility mode? And other changes.

--- mocha-parser.lua
+++ mocha-parser.lua
@@ -29,7 +29,7 @@ INALIABLE RIGHTS:
 script_name = "Aegisub-Mocha"
 script_description = "Mocha output parser for Aegisub"
 script_author = "torque"
-script_version = "0.0.0-0.1+2ABF41" -- no, I have no idea how this versioning system works either.
+script_version = "0.0.0-2.718281828" -- no, I have no idea how this versioning system works either.
 include("karaskel.lua")
 include("utils.lua") -- because it saves me like 5 lines of code this way
 gui = {}
@@ -47,7 +47,7 @@ gui.main = {
     label = "   Please enter a path to the mocha output. Can only take one file."},
   { class = "label";
       x = 0; y = 6; height = 1; width = 10;
-    label = "What tracking data should be applied?              Rounding"}, -- allows more accurate positioning >_>
+    label = "What tracking data should be applied?            Rounding"}, -- allows more accurate positioning >_>
   { class = "label";
       x = 0; y = 7; height = 1; width = 1;
     label = "Position:"},
@@ -93,7 +93,19 @@ gui.main = {
   { class = "textbox";
       x = 0; y = 12; height = 4; width = 10;
     name = "mocper"; hint = "Again, the full path to the file. No quotes or escapism needed.";
-    text = "I AM ACTUALLY SOMEWHAT SURPRISED THIS COMPILED, MUCH LESS RAN THIS FAR."}
+    text = "I AM ACTUALLY SOMEWHAT SURPRISED THIS COMPILED, MUCH LESS RAN THIS FAR."},
+  { class = "label";
+      x = 0; y = 16; height = 1; width = 3;
+    label = "Vsfilter Compatibility:"},
+  { class = "checkbox";
+      x = 3; y = 16; height = 1; width = 1;
+    value = true; name = "vsfilter"},
+  { class = "label";
+      x = 4; y = 16; height = 1; width = 1;
+    label = "Reverse:"},
+  { class = "checkbox";
+      x = 5; y = 16; height = 1; width = 1;
+    value = true; name = "reverse"}
 }
 
 gui.halp = {
@@ -105,8 +117,8 @@ gui.halp = {
 function prerun_czechs(sub, sel, act) -- for some reason, act always returns -1 for me.
   local strt
   for x = 1,#sub do -- so if there are like 10000 different styles then this is probably a really bad idea but I DON'T GIVE A FUCK
-    if string.find(sub[x].raw,"%[[E|e]vents%]") then -- BECAUSE I SAID SO
-      strt = x+1 -- start line of dialogue subs
+    if sub[x].class == "dialogue" then -- BECAUSE I SAID SO
+      strt = x -- start line of dialogue subs
       break
     end
   end
@@ -218,30 +230,22 @@ end
 
 function init_input(sub,accd) -- THIS IS PROPRIETARY CODE YOU CANNOT LOOK AT IT
   gui.main[2].text = accd.errmsg -- insert our error messages
-  local button, config = aegisub.dialog.display(gui.main, {"Go","Abort","Help","VSfilter"})
+  local button, config = aegisub.dialog.display(gui.main, {"Go","Abort","Help"})
   if button == "Go" then
     config.reverse = false -- since I haven't added it to the interface yet
     if config.reverse then
       aegisub.progress.title("slibreG gnicniM")
-      emarf_yb_emarf(sub,accd,config) -- really not sure if this is the best way to do this.
     else
       aegisub.progress.title("Mincing Gerbils")
-      frame_by_frame(sub,accd,config)
     end
+    frame_by_frame(sub,accd,config)
   elseif button == "Help" then
     aegisub.progress.title("Helping Gerbils?")
     help(sub,accd)
-  elseif button == "VSfilter" then
-    aegisub.progress.title("Incinerating Gerbils")
-    vshitler(sub,accd)
   else
     aegisub.progress.task("ABORT")
   end
   aegisub.set_undo_point("fan hitting the shit") -- I'm about 80% sure this is obsolete
-end
-
-function vshitler(sub,accd,opts)
-  error("seriously, why would you click that")
 end
 
 function help(su,ac)
@@ -250,7 +254,7 @@ function help(su,ac)
     init_input(su,ac)
   end
 end
-
+  
 function parse_input(infile)
   local ftab = {}
   local sect, care = 0, 0
@@ -290,7 +294,7 @@ function parse_input(infile)
     return mocha -- hurr durr
   else
     --return some system crippling error and wonder how the hell mocha's output is messed up
-    aegisub.log(0,"The mocha data is not internally equal length. Going into crash mode, t-10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0. Blast off.")
+    aegisub.log(0,"The mocha data is not internally equal length.")
     error("YOU HAVE FUCKED EVERYTHING UP")
   end
 end
@@ -300,6 +304,42 @@ function frame_by_frame(sub,accd,opts)
   assert(accd.totframes==mocha.flength,"Number of frames from selected lines differs from number of frames tracked.")
   local _ = nil
   local it = 1
+  if not opts.scl then
+    for k,d in ipairs(mocha.xscl) do
+      d = mocha.xscl[rstartf]
+      mocha.yscl[k] = mocha.yscl[rstartf] -- so that yscl is changed too. 
+    end
+  end
+  local operations = {} -- create a table and put the necessary functions into it, which will save a lot of if operations in the inner loop. This was the most elegant solution I came up with.
+  local eraser = {}
+  if opts.pos then
+    table.insert(operations, possify)
+    table.insert(eraser, "\\pos%([%-%d%.]+,[%-%d%.]+%)")
+  end
+  if opts.scl then
+    table.insert(eraser, "\\fscx[%d%.]+")
+    table.insert(eraser, "\\fscy[%d%.]+")
+    if opts.bord then
+      table.insert(eraser, "\\xbord[%d%.]+")
+      table.insert(eraser, "\\ybord[%d%.]+")
+      table.insert(eraser, "\\bord[%d%.]+")
+    end
+    if opts.shad then
+      table.insert(eraser, "\\xshad[%-%d%.]+")
+      table.insert(eraser, "\\yshad[%-%d%.]+")
+      table.insert(eraser, "\\shad[%-%d%.]+")
+    end
+    if opts.vsfilter then
+      table.insert(operations,VScalify)
+    else
+      table.insert(operations,scalify)
+    end
+  end
+  if opts.rot then
+    table.insert(operations,rotate)
+    table.insert(eraser, "\\org%([%-%d%.]+,[%-%d%.]+%)")
+    table.insert(eraser, "\\frz[%-%d%.]+")
+  end
   for i,v in ipairs(accd.lines) do
     local rstartf = v.startframe - accd.startframe + 1 -- start frame of line relative to start frame of tracked data
     local rendf = v.endframe - accd.startframe -- end frame of line relative to start frame of tracked data
@@ -311,55 +351,25 @@ function frame_by_frame(sub,accd,opts)
       v.xdiff, v.ydiff = mocha.xpos[rstartf] - v.xpos, mocha.ypos[rstartf] - v.ypos
     end
     local orgtext = v.text -- tables are passed as references.
-    if not opts.scl then
-      for k,d in ipairs(mocha.xscl) do
-        d = mocha.xscl[rstartf]
-        mocha.yscl[k] = mocha.yscl[rstartf]
-      end
-    end
-    if opts.pos then
-      v.text = string.gsub(v.text,"\\pos%([%-%d%.]+,[%-%d%.]+%)","") -- well, here it only has to run once per line.
-    end
-    if opts.scl then
-      v.text = string.gsub(v.text,"\\fscy[%d%.]+","")
-      v.text = string.gsub(v.text,"\\fscx[%d%.]+","")
-      if opts.bord then
-        v.text = string.gsub(v.text,"\\xbord[%d%.]+","")
-        v.text = string.gsub(v.text,"\\ybord[%d%.]+","")
-        v.text = string.gsub(v.text,"\\bord[%d%.]+","")
-      end
-      if opts.shad then
-        newtxt = string.gsub(newtxt,"\\xshad[%-%d%.]+","")
-        newtxt = string.gsub(newtxt,"\\yshad[%-%d%.]+","")
-        newtxt = string.gsub(newtxt,"\\shad[%-%d%.]+","")
-      end
-    end
-    if opts.rot then
-      newtxt = string.gsub(newtxt,"\\org%([%-%d%.]+,[%-%d%.]+%)","") -- idfklol
-      newtxt = string.gsub(newtxt,"\\frz[%-%d%.]+","")
+    for ie, ei in ipairs(eraser) do
+      v.text = string.gsub(v.text,ei,"")
     end
     if opts.pos and not v.xpos then
       aegisub.log(1,"Line %d is being skipped because it is missing a \\pos() tag and you said to track position. Moron.",v.num) -- yeah that should do it.
     else
-    local tag = "{"
-    local rat = {}
+      local tag = "{"
+      local rat = {}
       for x = rstartf,rendf do
-        rat[1] = mocha.xscl[iter]/mocha.xscl[rstart] -- DIVISION IS SLOW
-        rat[2] = mocha.yscl[iter]/mocha.yscl[rstart]
+        v.ratx = mocha.xscl[iter]/mocha.xscl[rstart] -- DIVISION IS SLOW
+        v.raty = mocha.yscl[iter]/mocha.yscl[rstart]
         v.start_time = aegisub.ms_from_frame(accd.startframe+x-1)
         v.end_time = aegisub.ms_from_frame(accd.startframe+x)
         v.text = pos_scl_rot(v,mocha,x,rstartf,opts)
-        if opts.pos then
-          tag = possify(v,mocha,x,rstartf,opts,tag,rat)
-        end
-        if opts.scl then
-          tag = scalify(v,mocha,x,rstartf,opts,tag,rat)
-        end
-        if opts.rot then
-          tag = rotate(v,mocha,x,rstartf,opts,tag)
+        for vk,kv in ipairs(operations) do -- iterate through the necessary operations
+          tag = tag..kv(v,mocha,x,rstartf,opts,tag)
         end
         tag = tag.."}"
-        v.text = tag..v.text
+        v.text = tag..v.text -- insert the new tags in a separate block before the 
         sub.insert(v.num+it,v)
         it = it + 1
         v.text = orgtext
@@ -369,19 +379,19 @@ function frame_by_frame(sub,accd,opts)
 end
 
 function possify(line,mocha,iter,rstart,opts,tag,rat)
-  local xpos = mocha.xpos[iter]-(line.xdiff*rat[1])
-  local ypos = mocha.ypos[iter]-(line.ydiff*rat[2]) 
+  local xpos = mocha.xpos[iter]-(line.xdiff*v.ratx)
+  local ypos = mocha.ypos[iter]-(line.ydiff*v.raty) 
   tag = tag..string.format("\\pos(%g,%g)",round(xpos,opts.pround),round(ypos,opts.pround))
   return tag
 end
 
 function scalify(line,mocha,iter,rstart,opts,tag,rat)
-  local xscl = line.xscl*rat[1]
-  local yscl = line.yscl*rat[2]
+  local xscl = line.xscl*v.ratx
+  local yscl = line.yscl*v.raty
   tag = tag..string.format("\\fscx%g\\fscy%g",round(xscl,opts.sround),round(yscl,opts.sround))
   if opts.bord then -- there's no nonretarded way to do this is there
-    local xbord = line.xbord*round(rat[1],opts.sround) -- round beforehand to minimize random float errors
-    local ybord = line.ybord*round(rat[2],opts.sround) -- or maybe that's rly fucking dumb? idklol
+    local xbord = line.xbord*round(v.ratx,opts.sround) -- round beforehand to minimize random float errors
+    local ybord = line.ybord*round(v.raty,opts.sround) -- or maybe that's rly fucking dumb? idklol
     if xbord == ybord then
       tag = tag..string.format("\\bord%g",round(xbord,opts.sround))
     else
@@ -389,8 +399,38 @@ function scalify(line,mocha,iter,rstart,opts,tag,rat)
     end
   end
   if opts.shad then
-    local xshad = line.xshad*round(rat[1],opts.sround) -- scale shadow the same way as everything else
-    local yshad = line.yshad*round(rat[2],opts.sround) -- hope it turns out as desired
+    local xshad = line.xshad*round(v.ratx,opts.sround) -- scale shadow the same way as everything else
+    local yshad = line.yshad*round(v.raty,opts.sround) -- hope it turns out as desired
+    if xshad == yshad then
+      tag = tag..string.format("\\shad%g",round(xshad,opts.sround))
+    else
+      tag = tag..string.format("\\xshad%g\\yshad%g",round(xshad,opts.sround),round(yshad,opts.sround))
+    end  
+  end
+  return tag
+end
+
+function VScalify(line,mocha,iter,rstart,opts,tag,rat)
+  local xscl = round(line.xscl*line.ratx,2)
+  local yscl = round(line.yscl*line.raty,2)
+  local xlowend, xhighend, xdecimal = math.floor(xscl),math.ceil(xscl),xscl%1*100
+  local xstart, xend = -xdecimal, 100-xdecimal
+  tag = tag..string.format("\\fscx%d\\t(%d,%d,\\fscx%d)",xlowend,xstart,xend,xhighend)
+  local ylowend, yhighend, ydecimal = math.floor(yscl),math.ceil(yscl),yscl%1*100
+  local ystart, yend = -ydecimal, 100-ydecimal
+  tag = tag..string.format("\\fscy%d\\t(%d,%d,\\fscy%d)",ylowend,ystart,yend,yhighend)
+  if opts.bord then -- there's no nonretarded way to do this is there
+    local xbord = line.xbord*round(v.ratx,opts.sround) -- round beforehand to minimize random float errors
+    local ybord = line.ybord*round(v.raty,opts.sround) -- or maybe that's rly fucking dumb? idklol
+    if xbord == ybord then
+      tag = tag..string.format("\\bord%g",round(xbord,opts.sround))
+    else
+      tag = tag..string.format("\\xbord%g\\ybord%g",round(xbord,opts.sround),round(ybord,opts.sround))
+    end
+  end
+  if opts.shad then
+    local xshad = line.xshad*round(v.ratx,opts.sround) -- scale shadow the same way as everything else
+    local yshad = line.yshad*round(v.raty,opts.sround) -- hope it turns out as desired
     if xshad == yshad then
       tag = tag..string.format("\\shad%g",round(xshad,opts.sround))
     else
