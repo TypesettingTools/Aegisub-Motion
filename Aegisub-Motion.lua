3e5a7a1175552e2e3472fbbb8ca92ad519fb352d
torque <lulzbot3000@gmail.com>
2011-12-13 02:07:43 -0800

    Reading from the subtitle header seems to work.

--- Aegisub-Motion.lua
+++ Aegisub-Motion.lua
@@ -298,6 +298,11 @@ function init_input(sub,accd) -- THIS IS PROPRIETARY CODE YOU CANNOT LOOK AT IT
   --]]
   local rand = ((os.clock()*os.time()+os.clock())*100) -- I suppose it's bad if this gives more variation than does math.random().
   gui.main[3].text = gui.motd[math.floor(rand%4)+1] -- this would work a lot better with more than 4 items
+  -- first, load defaults from file, then from script header, so the heirarchy goes: hardcoded -> config file -> script header
+  -- readfileconf() -- gonna need a separate function because it's a separate format.
+  if ourkeys["aa-mou-ReadConf"]:lower():match("true") then
+    headersettings(ourkeys) -- requires gui.main to be a global variable for now, I guess
+  end
   local button, config = aegisub.dialog.display(gui.main, {"Go","Abort","Help"})
   if button == "Go" then
     local confshort = {
@@ -338,15 +343,63 @@ function check_head(sub)
 		local l = sub[i]
     if l.class == "info" then
       if l.key:match("aa%-mou") then
-        aegisub.log(0,string.format("[i] = %s: %s\n",tostring(i),tostring(l.key),tostring(l.value)))
-        keytab[l.key] = l.value..":"..tostring(i) -- really not sure how I want to structure this
+        --aegisub.log(0,string.format("[%d] = %s: %s\n",tostring(i),tostring(l.key),l.value:match(" (.+)")))
+        keytab[l.key] = l.value:match(" (.+)")..":"..tostring(i) -- really not sure how I want to structure this
       end
     end
   end
   return keytab
 end
 
---[[function doThingsLikeWriteTheConfigurationToTheScriptWhileSuddenlyAdoptingOOStyleMethodNomenclature(TableOfTheCollectedOptions,TheSubtitlesObjectToWriteTo,aTableOfRelevantHeaderKeyValuePairs)
+function headersettings(hkeys) -- this function is hideous garbage.
+  for k,v in pairs(hkeys) do
+    local a = v:split()
+    --aegisub.log(0,"%s\n",a[1])
+    if k == "aa-mou-Position" then
+      gui.main[8].value = a[1]:tobool()
+    end
+    if k == "aa-mou-Scale" then
+      gui.main[10].value = a[1]:tobool()
+    end
+    if k == "aa-mou-Border" then
+      gui.main[12].value = a[1]:tobool()
+    end
+    if k == "aa-mou-Shadow" then
+      gui.main[14].value = a[1]:tobool()
+    end
+    if k == "aa-mou-Rotation" then
+      gui.main[16].value = a[1]:tobool()
+    end
+    if k == "aa-mou-ReadConf" then
+      gui.main[21].value = a[1]:tobool()
+    end
+    if k == "aa-mou-VSCompat" then
+      gui.main[23].value = a[1]:tobool()
+    end
+    if k == "aa-mou-Reverse" then
+      gui.main[25].value = a[1]:tobool()
+    end
+    if k == "aa-mou-PRound" then
+      gui.main[17].value = tonumber(a[1])
+    end
+    if k == "aa-mou-SRound" then
+      gui.main[18].value = tonumber(a[1])
+    end
+    if k == "aa-mou-RRound" then
+      gui.main[19].value = tonumber(a[1])
+    end
+  end
+end
+
+function string:tobool() -- uh.............
+  if self:lower() == "true" then
+    return true
+  else
+    return false
+  end
+end
+
+--[[function doWriteTheCurrentConfigurationToTheScriptAbusingOOStyleNamingConventions(TableOfTheCollectedOptions,TheSubtitlesObjectToWriteTo,aTableOfRelevantHeaderKeyValuePairs)
   -- if no known values, always insert at line 4 of the subtitles object
   for theKeysToTheTableOfTheOptions, theValuesThatCorrespondToTheKeys in pairs(TableOfTheCollectedOptions) do
     local theKeysToTheTableOfTheOptionsWithTheRelevantPrefixAppendedToThem = "aa-mou-"..theKeysToTheTableOfTheOptions
