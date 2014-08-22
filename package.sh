3f4502c50ca4a1c468cea391bb9b095b0c6faaf2
torque <torque@1>
2014-08-21 19:32:58 -0700

    Swankify packaging script.

--- package.sh
+++ package.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env sh
 
-git stash
-git checkout $1
-mkdir -p release/include/a-mo
-mkdir -p release/autoload
-cp -R src/ release/include/a-mo
-cp -R inc/luajson/lua/ release/include
-cp Install.txt release
-zip aegisub-motion-$1 release
-git checkout master
-git stash pop
+if [ -n "$1" ]; then
+	git stash > /dev/null
+	git checkout $1
+	mkdir -p aegisub-motion$1/include/a-mo
+	mkdir -p aegisub-motion$1/autoload
+	cp -R src/ aegisub-motion$1/include/a-mo
+	cp -R inc/luajson/lua/ aegisub-motion$1/include
+	cp Aegisub-Motion.moon aegisub-motion$1/autoload
+	cp Install.txt aegisub-motion$1
+	zip -r aegisub-motion-$1.zip aegisub-motion$1
+	rm -rf aegisub-motion$1
+	git checkout master
+	git stash pop > /dev/null
+else
+	echo "You gotta specify a tag."
+fi
